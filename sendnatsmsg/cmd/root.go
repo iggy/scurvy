0afb36d5a32f2fb29e698c88a6210b88bada78de
iggy <iggy@theiggy.com>
2018-04-30 03:31:18 +0200

    Add binary to send test messages
    Add binary to send test messages mostly for development purposes. I also
    sometimes use it to coax scurvy to sync files

---
+++ sendnatsmsg/cmd/root.go
@@ -0,0 +1,73 @@
+// Copyright © 2018 Iggy <iggy@theiggy.com>
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "sendnatsmsg",
+	Short: "Send test messages to the NATS message bus for scurvy",
+	Long: `Just a simple app to send some test messages to NATS to probe various bits of
+scurvy functionality.
+
+i.e. send a message to fake a new download, send new files, etc...`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (uses same config as the rest of scurvy)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		viper.AddConfigPath(home)
+		viper.SetConfigName("scurvy")
+		viper.AddConfigPath("/etc/")
+		viper.AddConfigPath(".")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ sendnatsmsg/cmd/testnewdownload.go
@@ -0,0 +1,53 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"scurvy/config"
+	"scurvy/msgs"
+
+	"github.com/spf13/cobra"
+)
+
+// testnewdownloadCmd represents the testnewdownload command
+var testnewdownloadCmd = &cobra.Command{
+	Use:   "testnewdownload",
+	Short: "Send new download test message",
+	Long:  `Sends a test message that mimics the message received from the webhook listener`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("testnewdownload called")
+		config.ReadConfig()
+		msg := msgs.NewDownload{Name: "Test", Path: "/scurvy"}
+
+		msgs.SendNatsMsg(msgs.NewDownloadSubject, msg)
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(testnewdownloadCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// testnewdownloadCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// testnewdownloadCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}

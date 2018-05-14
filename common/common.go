25fa791ba22b2f94473c2ec1ead1d2278d2c908a
iggy <iggy@theiggy.com>
2018-05-14 05:09:11 +0200

    add common code

---
+++ common/common.go
@@ -0,0 +1,13 @@
+package common
+
+import (
+	"log"
+)
+
+// CheckErr - check error code and pani
+// This is mostly to deduplicate code across all the files
+func CheckErr(err error) {
+	if err != nil {
+		log.Panicln(err)
+	}
+}

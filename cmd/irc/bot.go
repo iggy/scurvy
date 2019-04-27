fa20b638dd4ea494f9f8b8b8fcb0d248577f57e7
iggy <iggy@theiggy.com>
2019-04-27 13:51:34 -0700

    debugging running via docker (with no stdin)

--- cmd/irc/bot.go
+++ cmd/irc/bot.go
@@ -19,8 +19,10 @@ import (
 )
 
 var ircServername = "irc.oftc.net"
-var ircPort = 6697
+var ircPort = 6697 // ssl port
 var ircChannelname = "#testscurvybot"
+
+var reallyquit = false
 
 func main() {
 	log.Println("Initializing scurvy ircbot")
@@ -35,17 +37,21 @@ func main() {
 	cfg.Server = fmt.Sprintf("%s:%d", ircServername, ircPort)
 	cfg.NewNick = func(n string) string { return n + "^" }
 	// different Recover function that exits (vs just logging)
-	cfg.Recover = func(conn *irc.Conn, line *irc.Line) {
-		log.Panicln("Error in irc handler. Hopefully there's a useful error above.")
-	}
+	// cfg.Recover = func(conn *irc.Conn, line *irc.Line) {
+	// 	log.Printf("%v\n", conn)
+	// 	log.Printf("%v\n", line)
+	// 	log.Println("Error in irc handler. Hopefully there's a useful error above.")
+	// }
 	c := irc.Client(cfg)
 	c.EnableStateTracking()
 
 	// setup handlers
 	// join channel on connect
+	// log.Println("Setting up connect/join handler")
 	c.HandleFunc(irc.CONNECTED,
 		func(conn *irc.Conn, line *irc.Line) { conn.Join(ircChannelname) })
 
+	// log.Println("Setting up privmsg handler")
 	c.HandleFunc(irc.PRIVMSG, handleprivmsg)
 
 	// signal on disconnect
@@ -54,24 +60,32 @@ func main() {
 		func(conn *irc.Conn, line *irc.Line) { quit <- true })
 
 	// read commands on stdin
+	// log.Println("Setting up stdin reader")
 	in := make(chan string, 4)
-	reallyquit := false
-	go func() {
-		con := bufio.NewReader(os.Stdin)
-		for {
-			s, err := con.ReadString('\n')
-			if err != nil {
-				log.Println("Error on ReadString, not reading from stdin anymore.")
-				close(in)
-				break
-			}
-			log.Printf("stdin: %s", s)
-			if len(s) > 2 {
-				in <- s[0 : len(s)-1]
-			}
-		}
-	}()
+	stats, statErr := os.Stdin.Stat()
+	if statErr != nil {
+		fmt.Println("file.Stat()", statErr)
+	}
 
+	if stats.Size() > 0 {
+		go func() {
+			// log.Println("Setting up stdin for loop")
+
+			con := bufio.NewReader(os.Stdin)
+			for {
+				s, _, err := con.ReadLine()
+				if err != nil {
+					log.Println("Error on ReadString, not reading from stdin anymore.")
+					close(in)
+					break
+				}
+				log.Printf("stdin: %s", s)
+				if len(s) > 2 {
+					in <- string(s)
+				}
+			}
+		}()
+	}
 	// another goroutine for parsing stdin
 	go func() {
 		for cmd := range in {
@@ -108,13 +122,15 @@ func main() {
 	}()
 
 	// setup NATS queue watcher to send IRC message on new download
+	log.Println("Setting up NATS connection")
 	nc, err := nats.Connect(config.GetNatsConnString(),
 		nats.UserInfo(viper.GetString("mq.user"), viper.GetString("mq.password")))
-	common.CheckErr(err)
+	errors.CheckErr(err)
 	natschan, err := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
-	common.CheckErr(err)
+	errors.CheckErr(err)
 
 	subj, i := "scurvy.notify.*", 0
+	log.Println("Setting up subscription")
 	natschan.Subscribe(subj, func(msg *nats.Msg) {
 		i++
 		log.Printf("[#%d] Received on [%s]: '%s'\n", i, msg.Subject, string(msg.Data))
@@ -131,8 +147,9 @@ func main() {
 	natschan.Flush()
 
 	lerr := nc.LastError()
-	common.CheckErr(lerr)
+	errors.CheckErr(lerr)
 
+	log.Println("Setting up reallyquit loop")
 	for !reallyquit {
 		// connect
 		if err := c.Connect(); err != nil {
@@ -146,17 +163,42 @@ func main() {
 
 }
 
+// parseArgs - parse the irc args into a command and args to that command
+func parseArgs(a string) (string, interface{}) {
+	if len(a) < 2 {
+		log.Printf("Got invalid line: %v\n", a)
+		return a, nil
+	}
+
+	split := strings.SplitN(a, " ", 2)
+
+	if len(split) < 2 {
+		return a, ""
+	}
+
+	return split[0], split[1]
+}
+
 func handleprivmsg(conn *irc.Conn, line *irc.Line) {
-	// This is similar to the inline handler functions, but it's broken out here because of length
 	log.Printf("privmsg: %s\n", line)
 	log.Printf("args: %s\n", line.Args)
-	log.Println(conn)
-	switch line.Args[1] {
+	// log.Println(conn)
+
+	retChan := line.Args[0]
+
+	command, cargs := parseArgs(line.Args[1])
+	switch command {
 	case "^search":
-		conn.Privmsg(ircChannelname, "Search not implemented yet")
+		log.Printf("Got search command with args: %v\n", cargs)
+		conn.Privmsg(retChan, "Search not implemented yet")
 	case "^help":
-		conn.Privmsg(ircChannelname, "^help: this message")
-		conn.Privmsg(ircChannelname, "^search: search for media")
+		conn.Privmsg(retChan, "^help:    this message")
+		conn.Privmsg(retChan, "^search:  search for media")
+	case "^quit":
+		log.Println("Got shutdown command. Bye!")
+		conn.Privmsg(retChan, "Fine. I know when I'm not wanted")
+		reallyquit = true
+		conn.Quit("Got ^quit IRC command")
 	default:
 		log.Printf("line: %s\n", line)
 		log.Printf("args: %s\n", line.Args)

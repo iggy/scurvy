936a0ad998de5fb9b182a9f601be1b45acb50c34
iggy <iggy@kws1.com>
2020-04-20 05:38:36 +0000

    Add diskfull message handler
    Add the ability to send the disk full message to slack (we were already
    sending the message from input-webhook to Nats). Also fix a bug in
    faileddownload handling (it was unmarshalling into newdownload struct,
    which just happened to work because the structs are currently the same)

--- cmd/notifyd/scurvy-notify.go
+++ cmd/notifyd/scurvy-notify.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"runtime"
-
-	"encoding/json"
 
 	"github.com/iggy/scurvy/pkg/config"
 	"github.com/iggy/scurvy/pkg/errors"
@@ -13,25 +12,34 @@ import (
 	"github.com/iggy/scurvy/pkg/notify"
 
 	"github.com/nats-io/go-nats"
-	// "github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 func handleNatsMsg(m *nats.Msg) {
 	log.Printf("Received on [%s]: '%s'\n", m.Subject, string(m.Data))
-	var jmsg = msgs.NewDownload{}
-	if jerr := json.Unmarshal(m.Data, &jmsg); jerr != nil {
-		log.Panicf("fatal error reading json msg from nats: %s", jerr)
-	}
 	if m.Subject == "scurvy.notify.newdownload" {
+		var jmsg = msgs.NewDownload{}
+		if jerr := json.Unmarshal(m.Data, &jmsg); jerr != nil {
+			log.Panicf("fatal error reading NewDownload json msg from nats: %s", jerr)
+		}
 		notify.SendGeneralSlack(fmt.Sprintf("Good news everyone! %s was downloaded to %s",
 			jmsg.Name, jmsg.Path))
 	}
 	if m.Subject == "scurvy.notify.faileddownload" {
+		var jmsg = msgs.FailedDownload{}
+		if jerr := json.Unmarshal(m.Data, &jmsg); jerr != nil {
+			log.Panicf("fatal error reading FailedDownload json msg from nats: %s", jerr)
+		}
 		notify.SendGeneralSlack(fmt.Sprintf("Bad news everyone! %s failed to downloaded to %s",
 			jmsg.Name, jmsg.Path))
 	}
-	// log.Printf("%s - %v - %v", m.Subject, m.Reply, m.Sub)
+	if m.Subject == "scurvy.notify.diskfull" {
+		var jmsg = msgs.DiskFull{}
+		if jerr := json.Unmarshal(m.Data, &jmsg); jerr != nil {
+			log.Panicf("fatal error reading DiskFull json msg from nats: %s", jerr)
+		}
+		notify.SendAdminSlack(fmt.Sprintf("Disk full: %s", jmsg.Message))
+	}
 }
 
 func main() {
@@ -51,7 +59,7 @@ func main() {
 
 	lerr := nc.LastError()
 	errors.CheckErr(lerr)
-	// sendAdminSlack("Initializing Scurvy Notification Daemon.")
+	notify.SendAdminSlack("Initializing Scurvy Notification Daemon.")
 
 	runtime.Goexit()
 }
